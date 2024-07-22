25077bf139753940923c9ab0ecd5a05dce369107
keguigong <keguigong@gmail.com>
2024-07-23 00:08:03 +0800

    init: initial commit

---
+++ DinoScreenSaver/DinoScreenSaverView.h
@@ -0,0 +1,12 @@
+//
+//  DinoScreenSaverView.h
+//  DinoScreenSaver
+//
+//  Created by Jiahong HE on 23/7/2024.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface DinoScreenSaverView : ScreenSaverView
+
+@end
---
+++ DinoScreenSaver/DinoScreenSaverView.m
@@ -0,0 +1,51 @@
+//
+//  DinoScreenSaverView.m
+//  DinoScreenSaver
+//
+//  Created by Jiahong HE on 23/7/2024.
+//
+
+#import "DinoScreenSaverView.h"
+
+@implementation DinoScreenSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
